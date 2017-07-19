f453f175a0594657d8ee379fbf1e59815f3ad956
SURAMERICANA\carlvarn <1037636590>
2017-07-19 18:22:43 -0500

    Test and Solution

--- src/main/java/com/codewars/Kata.java
+++ src/main/java/com/codewars/Kata.java
@@ -1,11 +1,17 @@
 package com.codewars;
 
+import java.util.stream.IntStream;
+
 /**
  * Created by carlvarn on 19/07/2017.
  */
 public class Kata {
     public static int sum(int[] numbers)
     {
-        return 0;
+        int sum = 0;
+        if(numbers != null && numbers.length > 1){
+            sum = IntStream.of(numbers).sorted().skip(1L).map(i -> -i).sorted().map(i -> -i).skip(1L).sum();
+        }
+        return sum;
     }
 }
--- src/test/java/com/codewars/SolutionTest.java
+++ src/test/java/com/codewars/SolutionTest.java
@@ -12,4 +12,28 @@ public class SolutionTest {
     public void BasicTests() {
         assertEquals(16, Kata.sum(new int[] { 6, 2, 1, 8, 10}));
     }
+    @Test
+    public void NullTests() {
+        assertEquals(0, Kata.sum(null));
+    }
+    @Test
+    public void EmptyTests() {
+        assertEquals(0, Kata.sum(new int[] {}));
+    }
+    @Test
+    public void Test1() {
+        assertEquals(6, Kata.sum(new int[] { 1, 1, 11, 2, 3}));
+    }
+    @Test
+    public void Test2() {
+        assertEquals(16, Kata.sum(new int[] { 1, 11, 11, 2, 3}));
+    }
+    @Test
+    public void Test3() {
+        assertEquals(5, Kata.sum(new int[] { 1, 5, 11}));
+    }
+    @Test
+    public void Test4() {
+        assertEquals(17, Kata.sum(new int[] { 1, 1, 5, 11, 11}));
+    }
 }
